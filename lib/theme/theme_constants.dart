b8ac0a0cdd45fc21864cb08c8da97d5e1aa04fbd
mbenfe <benfeghoul@btinternet.com>
2023-05-25 09:31:21 +0200

    reverse

--- lib/theme/theme_constants.dart
+++ lib/theme/theme_constants.dart
@@ -1,16 +1,21 @@
 // ignore_for_file: constant_identifier_names, non_constant_identifier_names
 
+import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
 
-ThemeData lightTheme = ThemeData(
-  brightness: Brightness.light,
-  colorSchemeSeed: Colors.blue,
-  useMaterial3: true,
-);
+ThemeData lightTheme = FlexThemeData.light(scheme: FlexScheme.mandyRed);
 
-ThemeData darkTheme = ThemeData(
-  brightness: Brightness.dark,
-//  colorScheme: const ColorScheme.dark(),
-  colorSchemeSeed: Colors.white,
-  useMaterial3: true,
-);
+ThemeData darkTheme = FlexThemeData.dark(scheme: FlexScheme.mandyRed);
+
+// ThemeData lightTheme = ThemeData(
+//   brightness: Brightness.light,
+//   colorSchemeSeed: Colors.blue,
+//   useMaterial3: true,
+// );
+
+// ThemeData darkTheme = ThemeData(
+//   brightness: Brightness.dark,
+// //  colorScheme: const ColorScheme.dark(),
+//   colorSchemeSeed: Colors.white,
+//   useMaterial3: true,
+// );
